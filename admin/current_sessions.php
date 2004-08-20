a4e6e20a3be2fce89f9e48217227ac8ecf45ac53
kimdom <kimdom>
2004-08-20 16:22:23 +0000

    this file shows count distinct sessions for numsession in descending order.
    Also modify so that Columbia Server is down or not, and shows columbia delay in response time.

--- admin/current_sessions.php
+++ admin/current_sessions.php
@@ -19,12 +19,15 @@ while($r  = mysql_fetch_assoc($res))
 <table border=1 width=90% align=center>
 <tr>
 	<td width=12%>
-	<tt><b><u>Ip Address:</u></b></tt></td>
+	<tt><b><a href="../admin/ip_sessions.php">Ip Address:</a></b></tt>
+	</td>
 	<td width=18%><tt><b><u>Creation Time:</u></b></tt></td>
 	<td width=22%><tt><b><u>Session Key:</u></b></tt></td>
 	<td width=23%><tt><b><u>Request URL:<br><sub>http://psychinformatics.nyspi.org/infobuttons?</sub></u></b></tt></td>
+	<td width=18%><tt><b><u>Columbia Replied?:</u></b></tt></td>
+	<td width=22%><tt><b><u>Processing Time (milliseconds):</u></b></tt></td>
 	<td width=25%><tt><b><u>Redirect URL:<br><sub>http://infobuttons.dbmi.columbia.edu/infobuttons/cgi-bin/wc_infomanage.cgi?info_institute=NYOMH&info_context=InPatientDrugs&</sub></u></b></tt></td>
-	</tr>
+</tr>
 
 <?php
 	foreach($sessions as $s)
@@ -44,12 +47,13 @@ while($r  = mysql_fetch_assoc($res))
 
 */
 
-
 		echo "<tr>\t
 		<td><tt>$ip</tt></td>
 		<td><tt>$access_time</tt></td>
 		<td><tt>$session_key</tt></td>
 		<td><tt>$request_uri</tt></td>
+		<td><tt>$Columbia_Reply</tt></td>
+		<td><tt>$processing_time</tt></td>
 		<td><tt>$redirect_url</tt></td>
 		</tr>\n";
 	}
