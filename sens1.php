64d9ae06c8992204fd6eefb6f6db08f9bf0bf6f9
kimdom <kimdom>
2004-11-16 19:41:42 +0000

    11/10 added three more query strings in mysql command for drug interaction

--- sens1.php
+++ sens1.php
@@ -5,7 +5,7 @@ session_start();
 $ip = getenv('REMOTE_ADDR');
 require_once("/var/www/html/infobuttons/inc/conn.php");
 
-$query = "insert into sessions (ip, access_time, session_key, request_uri) values('$ip', sysdate(), '" . session_id() . "','". getenv("QUERY_STRING") . "')";
+$query = "insert into sessions (ip, access_time, session_key, request_uri, redirect_url, processing_time, columbia_reply) values('$ip', sysdate(), '" . session_id() . "','". getenv("QUERY_STRING") . "','$redirect_url', $processing_time, '$columbia_reply')";
 
 
 mysql_query($query) or die(mysql_error());
